79aa60c85801a27f9ac40a8e84190077cea40a1c
ggrove87 <gwg1387@gmail.com>
2022-04-05 09:00:43 -0500

    added additional functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,58 @@
-let startQuiz = document.querySelector("#startQuiz");
+let startBtn = document.querySelector("#startQuiz");
 let timerEl = document.querySelector("#timer");
 let infoPage = document.querySelector(".card")
-let quizQuestions = document.querySelector("#quizBody");
+let quiz = document.querySelector("#quizBody");
+let quizQuestion = document.querySelector("#quizQuestion");
+let answers = document.querySelector("#answers");
+let answer1 = document.querySelector("#answer1");
+let answer2 = document.querySelector("#answer2");
+let answer3 = document.querySelector("#answer3");
+let answer4 = document.querySelector("#answer4");
+let finalScore = document.querySelector("#finalScore");
 let timer = 60;
 let initialScore = 0;
+let selectedQuestion = 0;
 
-questionInfo = [{ question: "What is CSS?", answers: { a: "Cascasding Style Sheet", b: "Crictical Style Sheet", c: "Cascading Styling Sheet", d: "Create Styles Sheet" }, correctAnswer: "a" },
-{ question: "Commonly used data types do NOT include:", answers: { a: "strings", b: "booleans", c: "alerts", d: "numbers" }, correctAnswer: "c" },
-{ question: "Arrays in JavaScript can be used to store what?", answers: { a: "numbers and strings", b: "booleans", c: "Other arrays", d: "All of the above" }, correctAnswer: "d" },
+questionInfo = [{ question: "What is CSS?", a: "Cascasding Style Sheet", b: "Crictical Style Sheet", c: "Cascading Styling Sheet", d: "Create Styles Sheet", correctAnswer: "a" },
+{ question: "Commonly used data types do NOT include:", a: "strings", b: "booleans", c: "alerts", d: "numbers", correctAnswer: "c" },
+{ question: "Arrays in JavaScript can be used to store what?", a: "numbers and strings", b: "booleans", c: "Other arrays", d: "All of the above", correctAnswer: "d" },
 { question: "The condition of an if/else statement is enclosed with what?", answers: { a: "quotes", b: "curly brackets", c: "parenthesis", d: "square brackets" }, correctAnswer: "b" },
-{ question: "A very useful tool used during development and debugging for printing content to the console:", answers: { a: "display.textContent", b: "terminal", c: "console.log", d: "print screen" }, correctAnswer: "c" }];
+{ question: "A very useful tool used during development and debugging for printing content to the console:", a: "display.textContent", b: "terminal", c: "console.log", d: "print screen", correctAnswer: "c" }];
 
-startQuiz.on("click", startQuiz);
+startBtn.addEventListener("click", startQuiz());
 
 
 
 function startQuiz() {
     infoPage.style.display = "none";
-    quizQuestions.sytle.display = "block";
+    quiz.style.display = "block";
     setInterval(function () {
         timer--;
         timerEl.textContent = timer;
     }, 1000)
+
+    currentQuestion();
 }
 
-function showResults() {
+function currentQuestion() {
+    let i = questionInfo[selectedQuestion]
+    question.innerHTML = i.question;
+    a.innerHTML = i.a;
+    b.innerHTML = i.b;
+    c.innerHTML = i.c;
+    d.innerHTML = i.d;
+}
 
+function checkAnser()
+if (questionInfo) {
+
+}
+function showResults() {
+    infoPage.style.display = "none";
+    quiz.style.display = "none";
+    finalScore.style.display = "block";
+
+    if(timer === 0 || questionInfo.length -1) {
+        
+    }
 }
