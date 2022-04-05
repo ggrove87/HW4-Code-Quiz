a14879521c29790f3c7f6f3d18100e750c45cfe1
ggrove87 <gwg1387@gmail.com>
2022-04-05 07:42:47 -0500

    updated index.html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,29 @@
 let startQuiz = document.querySelector("#startQuiz");
 let timerEl = document.querySelector("#timer");
-
+let infoPage = document.querySelector(".card")
+let quizQuestions = document.querySelector("#quizBody");
 let timer = 60;
+let initialScore = 0;
 
-questionInfo = [{question:"What is CSS?",answers: {a: "Cascasding Style Sheet",b:"Crictical Style Sheet", c:"Cascading Styling Sheet", d: "Create Styles Sheet"}, correctAnswer: "a"},
-{question:"What is CSS?",answers: {a: "Cascasding Style Sheet",b:"Crictical Style Sheet", c:"Cascading Styling Sheet", d: "Create Styles Sheet"}, correctAnswer: "a"}];
+questionInfo = [{ question: "What is CSS?", answers: { a: "Cascasding Style Sheet", b: "Crictical Style Sheet", c: "Cascading Styling Sheet", d: "Create Styles Sheet" }, correctAnswer: "a" },
+{ question: "Commonly used data types do NOT include:", answers: { a: "strings", b: "booleans", c: "alerts", d: "numbers" }, correctAnswer: "c" },
+{ question: "Arrays in JavaScript can be used to store what?", answers: { a: "numbers and strings", b: "booleans", c: "Other arrays", d: "All of the above" }, correctAnswer: "d" },
+{ question: "The condition of an if/else statement is enclosed with what?", answers: { a: "quotes", b: "curly brackets", c: "parenthesis", d: "square brackets" }, correctAnswer: "b" },
+{ question: "A very useful tool used during development and debugging for printing content to the console:", answers: { a: "display.textContent", b: "terminal", c: "console.log", d: "print screen" }, correctAnswer: "c" }];
+
+startQuiz.on("click", startQuiz);
+
+
 
 function startQuiz() {
-    setInterval(function(){
+    infoPage.style.display = "none";
+    quizQuestions.sytle.display = "block";
+    setInterval(function () {
         timer--;
-        timerEl.textContent = timer
-    },1000)
+        timerEl.textContent = timer;
+    }, 1000)
 }
 
-function showResults(){
+function showResults() {
 
 }
